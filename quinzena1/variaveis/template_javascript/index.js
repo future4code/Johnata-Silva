1145baab7db357b8d0d7945d1f704bc67a8c33be
John182 <johnatasantos@yahoo.com.br>
2021-09-08 20:32:51 -0300

    Exercicios

---
+++ quinzena1/variaveis/template_javascript/index.js
@@ -0,0 +1,32 @@
+//Exercício 1
+
+//a)
+//let nome = prompt('Digite seu nome')
+//b)
+//let idade = prompt('Digite sua idade')
+//c)
+//console.log(typeof nome, idade)
+
+//d) Não foi definido nenhum valor
+
+//f) Apareceu string e um number
+
+//g)
+
+//console.log("Olá" ,nome, "voce tem" ,idade, "anos")
+
+//Exercício 2
+
+//let pergunta1 = prompt("Voce esta bem?")
+//let pergunta2 = prompt("Sente frio?")
+//let pergunta3 = prompt("Esta usando roupa azul?")
+
+//console.log("Voce esta bem? -" ,pergunta1)
+//console.log("Sente frio? -" ,pergunta2)
+//console.log("Esta usando roupa azul? -" ,pergunta3)
+
+//Exercício 3
+
+//let a = c
+//let b = a
+//let c = b
--- semana0/projeto-onboarding/aprendizados.md
+++
@@ -1,4 +0,0 @@
-Aprendizados da Semana
-
-Nessa semana de onboarding da **Labenu**, aprendi a mexer no terminal, 
-no VSCode e aprimorar meus conhecimentos com o git.
